e961b69fb207c91c4be8b4709c5936502b833041
Dane Forslund <deign001@gmail.com>
2022-07-02 15:14:51 +0000

    new file 'modifying' added

---
+++ Gen-Range.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+def gen_range(stop, start=1, step=1):
+    num = start
+    while num <+ stop:
+        yield num
+        num += step
+        
---
+++ Loopy.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+
+colors = ['blue', 'green', 'red', 'purple']
+for color in colors:
+    print(color)
+    
