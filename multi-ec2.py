0a032b52e4ba701df4ee7ffe0844c66f563f63ba
Dane Forslund <deign001@gmail.com>
2022-07-08 21:20:43 +0000

    another file for Boto3

---
+++ multi-ec2.py
@@ -0,0 +1,9 @@
+import boto3
+
+ec2_resource=boto3.resource("ec2")
+
+ec2_resource.create_instances(ImageId='ami-0cff7528ff583bf9a',
+        InstanceType='t2.micro',
+    MaxCount=3, #this number will rep how many we launch
+        MinCount=3,
+        )
